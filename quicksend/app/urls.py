d115064af260c9b7e0f46b347170520cf8db371b
RajviKamani <114154585+RajviKamani@users.noreply.github.com>
2023-04-30 07:23:24 +0530

    yes

--- quicksend/app/urls.py
+++ quicksend/app/urls.py
@@ -3,4 +3,10 @@ from app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+     path('home', views.home, name='home'),
+    path('upload', views.upload, name='upload'),
+    path('about', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+
 ]
--- quicksend/app/views.py
+++ quicksend/app/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
-
+from app.models import Email
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+   return render(request,'home.html')
+
+def login(request):
+        
+        if request.method == "POST":
+           email = request.POST.get('email')
+           password = request.POST.get('password')
+    
+           login=Email(email=email, password=password)
+           login.save()
+           return render(request,"home.html")
+		
+           
+
+def signup(request):
+      if request.method == "POST":
+           email = request.POST.get('email')
+           password = request.POST.get('password')
+    
+           signup=Email(email=email, password=password)
+           signup.save()
+		
+           return render(request,"home.html")
+    
+
+def about(request):
+    return render(request, 'about.html')
+
+def upload(request):
+    return render(request, 'upload.html')
+
