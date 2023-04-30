6fbcd1268917e025d38ee1eaf0d603918771b63f
Devanshvora03 <devanshvora03@gmail.com>
2023-04-30 08:10:33 +0530

    yes

--- quicksend/app/admin.py
+++ quicksend/app/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
-
-# Register your models here.
-=======
 from app.models import Email
 # Register your models here.
 
 admin.site.register(Email)
->>>>>>> 8f085d4e74ea3952e53928203581291de5e9c25c
--- quicksend/app/urls.py
+++ quicksend/app/urls.py
@@ -3,19 +3,9 @@ from app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-<<<<<<< HEAD
-    path('home', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('upload', views.upload, name='upload'),
-]
-=======
      path('home', views.home, name='home'),
     path('upload', views.upload, name='upload'),
     path('about', views.about, name='about'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
-
 ]
->>>>>>> 8f085d4e74ea3952e53928203581291de5e9c25c
