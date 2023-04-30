3ecaaa230c3de913e05bc3bf17b0e4524e5ae360
Devanshvora03 <devanshvora03@gmail.com>
2023-04-30 07:20:40 +0530

    css

--- quicksend/app/urls.py
+++ quicksend/app/urls.py
@@ -3,4 +3,9 @@ from app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('upload', views.upload, name='upload'),
 ]
--- quicksend/app/views.py
+++ quicksend/app/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def login(request):
+    if request.method=="POST":
+        uname = request.POST['username']
+        pass1 = request.POST['password']
+
+        user = authenticate(username=uname, password=pass1)
+        
+        if user is not None:
+            login(request,user)
+            return render(request, 'home.html')
+        
+        else:
+            messages.error(request, "Bad Credentials")
+            return redirect('home')
+        
+    return render(request, 'login.html')
+
+def signup(request):
+    if request.method=="POST":
+        uname = request.POST['username']
+        pass1 = request.POST['password']
+
+        my_user = User.objects.create_user(uname,pass1)
+        my_user.uname = uname
+        my_user.pass1 = pass1
+
+        my_user.save()
+        messages.success(request, "User has been created successfully")
+
+        return redirect('login')
+
+    return render(request, 'signup.html')
+
+def upload(request):
+    return render(request, 'upload.html')
